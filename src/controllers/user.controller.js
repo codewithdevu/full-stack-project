0553b1876bd2546843e071a8d41e03c3bdedf554
codewithdevu <divyanshyadav8302@gmail.com>
2026-04-22 01:17:53 +0530

    Add the check middlware for image file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,9 +2,18 @@ import {asynchandler} from "../utils/asynchandler.js"
 
 
 const registerUser = asynchandler(async (req , res) =>{
-    res.status(200).json({
-        message:"Ok"
-    })
+    // get user details from frontend 
+    // validation - not empty
+    // check if user already exists : username, email 
+    // check for images , check for avatar 
+    // upload them to couldinary , avatar 
+    // create user object- create entry token field from response
+    // check for user creation 
+    // return response
+
+    const {email, username, fullName , password} = req.body
+    console.log("email: ", email);
+    
 })
 
 export {registerUser}
