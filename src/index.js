0737d92fe6fbd04355dc25d1dbb44038e4e2aa77
codewithdevu <divyanshyadav8302@gmail.com>
2026-04-23 01:06:25 +0530

    add the debugging part

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
-import connectDb from "./database/connection.js";
 import dotenv from "dotenv";
-import { app } from "./app.js";
 
 dotenv.config({
-    path: ".env"
-})
+    path: "./.env"
+});
+
+import connectDb from "./database/connection.js";
+import { app } from "./app.js";
+
 
 connectDb()
-.then(
-    
-    app.listen(process.env.PORT || 8000 , () => {
+.then(()=> {
+        app.listen(process.env.PORT || 8000 , () => {
         console.log(`server started at port: http://localhost:${process.env.PORT}`);
         
-    }),
+    });
     
     app.get("/" , (req , res) => {
         res.send("welocome to homepage")
-    }),
-)
+    });
+})
 .catch((err) => {
     console.log("mongo db connect ERROR: " , err);
     
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,8 +52,7 @@ userSchema.pre("save" , async function (next) {
 
     if(!this.isModified("password")) return next();
     
-    this.password =  await bcrypt.hash(this.password, 10)
-    next()
+    this.password =  await bcrypt.hash(this.password, 10);
 })
 
 userSchema.methods.isPasswordCorrect = async function (password) {
