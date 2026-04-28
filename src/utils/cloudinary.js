9b5ee342adc0dcedbb6c3263c280ae91893cf822
codewithdevu <divyanshyadav8302@gmail.com>
2026-04-29 01:47:59 +0530

    add deletecloudinaryfile utility

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -8,25 +8,44 @@ cloudinary.config({
 });
 
 
+const deleteOnCloudinary = async (url , resource_type = "image") => {
+  try {
+    if (!url) return null;
+    
+    const fileName = url.split("/").pop();
+    const publicId = fileName.split(".")[0];
+
+    const response = await cloudinary.uploader.destroy(publicId, {
+      resource_type: resource_type
+    })
+    return response;
+  } catch (error) {
+    console.log("Error: while deleting form cloudinar", error);
+    return null;
+  }
+}
+
+
 const uploadOncloudinary = async (localFilepath) => {
   try {
-    if(!localFilepath) return null;
+    if (!localFilepath) return null;
     // upload the file on cloudinary
     const response = await cloudinary.uploader.upload(localFilepath, {
       resource_type: "auto"
-    }) ;
+    });
     //file has been uploaded successfull
     // console.log("file is uploaded on cloudinary" , response.url);
     fs.unlinkSync(localFilepath)
     // console.log(response);
+
     return response;
-    
+
   } catch (error) {
     fs.unlinkSync(localFilepath) // remove the locally saved temporary file as the upload operation got failed
     return null;
-    
+
   }
-  
+
 }
 
-export {uploadOncloudinary};
+export { uploadOncloudinary , deleteOnCloudinary};
