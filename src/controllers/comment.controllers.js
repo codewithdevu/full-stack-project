7957cb01ecdbd7cd88831419bf9d992a2a258f2e
codewithdevu <divyanshyadav8302@gmail.com>
2026-04-28 16:24:44 +0530

    add the getallcomments conroller

--- src/controllers/comment.controllers.js
+++ src/controllers/comment.controllers.js
@@ -10,6 +10,54 @@ const getVideoComments = asyncHandler(async (req, res) => {
     // Todo: get all comments for a video
     const { videoId } = req.params
     const { page = 1, limit = 10 } = req.query
+
+    const videoIdIsValid = mongoose.isValidObjectId(videoId)
+
+    if (!videoIdIsValid) {
+        throw new ApiError(400, "videoId is required")
+    }
+
+    const convertId = mongoose.Types.ObjectId(videoId)
+
+    const skipValue = (page - 1)* limit
+
+    const myPipline = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(convertId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users" ,
+                localField: "owner",
+                foreignField: "_id",
+                as: "ownerDetails"
+            }
+        },
+        {
+            $addFields: {
+                owner: {
+                    $first: "$ownerDetails"
+                }
+            }
+        },
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+    ])
+
+
+    const options = {page: parseInt(page), limit:parseInt(limit)}
+
+    const result = await Comment.aggregatePaginate(myPipline , options)
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, result , "get video comments successfully"))
+
 })
 
 const addComment = asyncHandler(async (req, res) => {
