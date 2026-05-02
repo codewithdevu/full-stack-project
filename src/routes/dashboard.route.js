e45e25386522d67833bab9f0546203299eb504e0
codewithdevu <divyanshyadav8302@gmail.com>
2026-05-03 01:45:08 +0530

    dashboard, like , playlist router

---
+++ src/routes/dashboard.route.js
@@ -0,0 +1,12 @@
+import {Router} from "express"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+import { getChannelStatus, getChannelVideos } from "../controllers/dashboard.controller.js";
+ 
+const router = Router();
+
+router.use(verifyJWT);
+
+router.route("/stats").get(getChannelStatus);
+router.route("/videos").get(getChannelVideos);
+
+export default router
