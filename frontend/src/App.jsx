0bdb6d47b128d714231e5909f5ff1131604528e9
codewithdevu <divyanshyadav8302@gmail.com>
2026-05-05 19:52:59 +0530

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+
+function App() {
+  return (
+    <h1>Upload pages route here</h1>
+  )
+}
+
+export default App
