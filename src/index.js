e29c66bb690a0dc89cb89de8006a9833afae22e8
codewithdevu <divyanshyadav8302@gmail.com>
2026-05-19 02:09:56 +0530

    add deploy debug part

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import path from "path";
+import {app} from "./app.js";
 
 dotenv.config({
     path: "./.env"
@@ -25,3 +26,4 @@ connectDb()
     
 })
 
+export default app;
