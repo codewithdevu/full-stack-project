b79cba073b06f58b78d308cf316593c20410ddf4
codewithdevu <divyanshyadav8302@gmail.com>
2026-05-19 11:36:49 +0530

    Add the debug

--- frontend/src/api/apiConfig.js
+++ frontend/src/api/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "https://full-stack-project-eight-pi.vercel.app",
+    baseURL: "http://localhost:3000/api/v1",
     withCredentials: true,
 });
 
