e802cc1cfb501f6aa7dae6e952646e4ec9947517
codewithdevu <divyanshyadav8302@gmail.com>
2026-06-11 22:21:43 +0530

    fix(cors): loosen origin validator arrays to encompass comprehensive vercel domains

--- src/app.js
+++ src/app.js
@@ -35,7 +35,7 @@ app.use(cors({
 }));
 
 // Pre-flight OPTIONS hit handling
-app.options("*", cors());
+// app.options("*", cors());
 
 // ⚠️ PAYLOAD LIMITS FOR FULL VIDEO STREAMS HANDLING
 app.use(express.json({ limit: "100mb" }));
